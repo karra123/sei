86589591748faa978e5116ba4e8c0f354ce34b86
karra123 <karra123@gmail.com>
2016-10-15 10:14:58 -0400

    Initial File Addition

---
+++ DeDup.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class DeDup {
+	public int[] randomIntegers = {1,2,34,34,25,1,45,3,26,85,4,34,86,25,43,2,1,10000,11,16,19,1,18,4,9,3,
+									20,17,8,15,6,2,5,10,14,12,13,7,8,9,1,2,15,12,18,10,14,20,17,16,3,6,19,
+									13,5,11,4,7,19,16,5,9,12,3,20,7,15,17,10,6,1,8,18,4,14,13,2,11};
+
+	public int[] getDeDupedIntegers(int [] array){
+		return getDeDupedIntegers(IntStream.of(array));
+	}
+	public int[] getDeDupedIntegers(IntStream stream){
+		return stream.boxed().collect(Collectors.toSet()).stream().mapToInt(i->i).toArray();
+	}
+	
+	
+	public int[] getDeDupedIntegersWithOrder(int [] array){
+		return getDeDupedIntegersWithOrder(IntStream.of(array));		
+	}
+	public int[] getDeDupedIntegersWithOrder(IntStream stream){
+		return stream.boxed().collect(Collectors.toCollection(LinkedHashSet::new)).stream().mapToInt(i->i).toArray();		
+	}
+
+	
+	public int[] getDeDupedIntegersSorted(int [] array){
+		return getDeDupedIntegersSorted(IntStream.of(array));		
+	}	
+	public int[] getDeDupedIntegersSorted(IntStream stream){
+		return stream.boxed().collect(Collectors.toCollection(TreeSet::new)).stream().mapToInt(i->i).toArray();		
+	}
+	
+	public static void main(String [] args){
+		DeDup dd = new DeDup();
+		System.out.println("DeDeuped Integer Array");
+		Arrays.stream(dd.getDeDupedIntegers(dd.randomIntegers)).forEach(e -> System.out.printf("%d ", e));
+		System.out.println("\nDeDeuped Integer Array with Order");
+		Arrays.stream(dd.getDeDupedIntegersWithOrder(dd.randomIntegers)).forEach(e -> System.out.printf("%d ", e));
+		System.out.println("\nDeDeuped Integer Array Sorted");
+		Arrays.stream(dd.getDeDupedIntegersSorted(dd.randomIntegers)).forEach(e -> System.out.printf("%d ", e));
+	}
+}
