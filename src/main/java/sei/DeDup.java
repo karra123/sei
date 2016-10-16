926eb3cae6659ae0e4e45a34c1a3dc20b1a15839
karra123 <karra123@gmail.com>
2016-10-15 20:49:22 -0400

    Thread Safe
    Thread Safe

--- src/main/java/sei/DeDup.java
+++ src/main/java/sei/DeDup.java
@@ -1,6 +1,7 @@
 package sei;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
@@ -11,15 +12,15 @@ public class DeDup {
 			20,17,8,15,6,2,5,10,14,12,13,7,8,9,1,2,15,12,18,10,14,20,17,16,3,6,19,
 			13,5,11,4,7,19,16,5,9,12,3,20,7,15,17,10,6,1,8,18,4,14,13,2,11};
 
-	public int[] getDeDupedIntegers(int [] array){
-		return IntStream.of(array).boxed().collect(Collectors.toSet()).stream().mapToInt(i->i).toArray();
+	public synchronized int[] getDeDupedIntegers(int [] array){
+		return IntStream.of(array).boxed().collect(Collectors.toCollection(HashSet::new)).stream().mapToInt(i->i).toArray();
 	}
 
-	public int[] getDeDupedIntegersWithOrder(int [] array){
+	public synchronized int[] getDeDupedIntegersWithOrder(int [] array){
 		return IntStream.of(array).boxed().collect(Collectors.toCollection(LinkedHashSet::new)).stream().mapToInt(i->i).toArray();		
 	}
 	
-	public int[] getDeDupedIntegersSorted(int [] array){
+	public synchronized int[] getDeDupedIntegersSorted(int [] array){
 		return IntStream.of(array).boxed().collect(Collectors.toCollection(TreeSet::new)).stream().mapToInt(i->i).toArray();		
 	}
 
